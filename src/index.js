50e5a849c0b5d716629d3accc61ec7c3be357148
Benjamin Cheng <bencheng200@gmail.com>
2021-05-24 19:05:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -20,3 +20,24 @@ function addImage(picUrl) {
   newImage.src = picUrl;
   container.appendChild(newImage);
 }
+
+function loadBreeds() {
+  return fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(results => {
+      const breeds = Object.keys(results.message);
+      addBreeds(breeds);
+    });
+}
+
+function addBreeds(breeds) {
+  const ul = document.getElementById("dog-breeds");
+  breeds.forEach(breed => {
+    const li = document.createElement("li");
+    li.innerText = breed;
+    ul.appendChild(li);
+    li.addEventListener("click", function(event) {
+      event.target.style.color = "blue";
+    });
+  });
+}
